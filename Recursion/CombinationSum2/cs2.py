531cf7dcbaed7705d3e755de25da8d5a01028786
Abdulwahab938 <awiitb786@gmail.com>
2025-07-15 20:44:30 +0530

    combination sum 2

---
+++ Recursion/CombinationSum2/cs2.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        ds = []
+        candidates.sort()
+
+        def findCombination(ind: int, target: int):
+            if target == 0:
+                ans.append(ds[:])
+                return
+            for i in range(ind, len(candidates)):
+                if i > ind and candidates[i] == candidates[i - 1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                ds.append(candidates[i])
+                findCombination(i + 1, target - candidates[i])
+                ds.pop()
+
+
+        findCombination(0, target)
+        return ans
