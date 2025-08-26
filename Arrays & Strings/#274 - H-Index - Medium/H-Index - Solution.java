23921efa46c21df16de93a4f675993353ab46e22
AmanDeol7 <88588895+AmanDeol7@users.noreply.github.com>
2025-08-26 10:53:56 +0530

    Added java Solution

---
+++ Arrays & Strings/#274 - H-Index - Medium/H-Index - Solution.java
@@ -0,0 +1,33 @@
+// Sorting approach
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+        return 0;
+    }
+}
+
+// Counting approach
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] count = new int[n + 1];
+        for (int c : citations) {
+            count[Math.min(c, n)]++;
+        }
+        int total = 0;
+        for (int i = n; i >= 0; i--) {
+            total += count[i];
+            if (total >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
